d6e90a2221ef013e13ff39286b10187b33529716
ARIJSAYEED <arijsayeed@gmail.com>
2025-11-13 03:01:27 +0600

    complete the main req

--- src/Components/PopularGames.jsx
+++ src/Components/PopularGames.jsx
@@ -6,54 +6,51 @@ import EG from '../assets/eg.jpg'
 
 const PopularGames = () => {
 
-    // let [games, setGames] = useState([])
-
-    // useEffect(() => {
-
-    //     let data = fetch('/Popular.json')
-    //         .then(result => result.json())
-    //         .then(data => {
-    //             // console.log(data);
-    //             setGames(data)
-    //         })
-    // }, [])
 
     return (
         <div>
-            <div className='flex gap-3 my-10 px-5 min-h-screen'>
-                <div className='w-1/3 flex items-center'>
-                    <h1 className='text-5xl font-bold'>Most Popular <span className='text-8xl font-bold'>Games</span></h1>
+            <div className='flex justify-center items-center flex-col gap-5 my-10 px-5 min-h-screen'>
+                <div className='items-center text-center' >
+                <p className='text-xl font-semibold text-[#0099ff]'>Welcome to GameHub</p>
+                    <h1 className='text-5xl font-bold'>Our Most Popular Games</h1>
                 </div>
-                <div className='w-2/3'>
-                    <div className="carousel h-96 flex justify-center items-center">
-                        <div id="slide1" className="carousel-item relative w-full">
-                            <img
-                                src={HW}
-                                className="w-full" />
-                            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                                <a href="#slide4" className="btn btn-circle">❮</a>
-                                <a href="#slide2" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide2" className="carousel-item relative w-full">
-                            <img
-                                src={MC}
-                                className="w-full" />
-                            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                                <a href="#slide1" className="btn btn-circle">❮</a>
-                                <a href="#slide3" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide3" className="carousel-item relative w-full">
-                            <img
-                                src={EG}
-                                className="w-full" />
-                            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                                <a href="#slide2" className="btn btn-circle">❮</a>
-                                <a href="#slide4" className="btn btn-circle">❯</a>
-                            </div>
+                <div className="carousel w-80 h-auto">
+                    <div id="slide1" className="carousel-item relative w-full">
+                        <img
+                            src={HW}
+                            className="w-full" />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide4" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
                         </div>
                     </div>
+                    <div id="slide2" className="carousel-item relative w-full">
+                        <img
+                            src={MC}
+                            className="w-full" />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide1" className="btn btn-circle">❮</a>
+                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide3" className="carousel-item relative w-full">
+                        <img
+                            src={EG}
+                            className="w-full" />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide2" className="btn btn-circle">❮</a>
+                            <a href="#slide4" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    {/* <div id="slide4" className="carousel-item relative w-full">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+                            className="w-full" />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide3" className="btn btn-circle">❮</a>
+                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        </div>
+                    </div> */}
                 </div>
             </div>
         </div>
--- src/Components/TopRatedGames.jsx
+++ src/Components/TopRatedGames.jsx
@@ -17,11 +17,11 @@ const TopRatedGames = () => {
 
     return (
 
-        <div className='my-10 space-y-5'>
+        <div className='my-10 space-y-5 py-10 border-b border-t border-neutral-400'>
             <div>
                 <h1 className='text-4xl font-semibold text-center'>Top Rated Games</h1>
             </div>
-            <div className='grid grid-cols-3 gap-4'>
+            <div className='grid grid-cols-3 gap-4 '>
                 {
                     data.map(game => <TopRatedGamesCard key={game.id} game={game}></TopRatedGamesCard>)
                 }
